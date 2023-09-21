38e8012dc9633817306db038fad5f3b722932767
zdupre18 <126183811+zdupre18@users.noreply.github.com>
2023-09-20 21:48:28 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,3 +5,15 @@ from main import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    def test_multiply():
+    assert quadratic_multiply(BinaryNumber(6), BinaryNumber(10)) == 6*10
+    def test_multiply():
+    assert quadratic_multiply(BinaryNumber(4), BinaryNumber(8)) == 4*8
+    def test_multiply():
+    assert quadratic_multiply(BinaryNumber(11), BinaryNumber(5)) == 11*5
+
+def test_additon():
+    
+    
+
+    
